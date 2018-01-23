d99a26c68f9a7d2ded42d2b2ba043b391bab0635
冀峰 <jifeng@strongunion.com.cn>
2018-01-23 18:24:13 +0800

    first commit

---
+++ TestDemo/mylibrary/src/main/java/com/strongunion/mylibrary/CalculateUtil.java
@@ -0,0 +1,11 @@
+package com.strongunion.mylibrary;
+
+/**
+ * Created by admin on 2018/1/23.
+ */
+
+public class CalculateUtil {
+    public double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+}
